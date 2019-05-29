99588d6b0b7454b7fe28c69431449584f150ed73
ilias <iliasnadi@gmail.com>
2019-05-29 12:19:04 +0200

    ajout jeux 2 et modif variables

--- 00_algorithmique/01_python/variables.py
+++ 00_algorithmique/01_python/variables.py
@@ -3,11 +3,16 @@
 variable1 = 5 # integer > int qui symbolise un nombre entier
 variable2 = 3.2 # float > float qui symbolise un chiffre à virgule
 variable3 = "Coucou" # string > str qui symbolise du texte
+variable4 = True #boolean > bool symbolise une variable soit vrai 
+                 #soit faux
+variable5 = ["1er", "2eme", "3eme"] #list > une variable liste qui 
+                                    #comprend 3 string
+
 
 #Fonction qui permet d'afficher du texte 
 # à l'écran
-print("du texte", variable1, variable2, variable3)
-
+print("du texte", variable1, variable2, variable3, variable4)
+print( variable5[1] )
 #Fonction permettant de connaitre le type d'une variable
 print( type(variable1) )
 

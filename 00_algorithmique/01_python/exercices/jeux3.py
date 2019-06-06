8a88b2499a40f2f2fb60c42804332f005b53648a
ilias <iliasnadi@gmail.com>
2019-06-06 14:11:46 +0200

    Ajout code Matrice

---
+++ 00_algorithmique/01_python/exercices/jeux3.py
@@ -0,0 +1,7 @@
+import random
+import libJeu3
+
+print(libJeu3.liste_mots[0])
+libJeu3.pendu(5)
+reponse = libJeu3.masque("bon","")
+print(reponse)
---
+++ 00_algorithmique/01_python/liste_dico.py
@@ -0,0 +1,33 @@
+maListe= []
+texte = "kayak"
+
+print(maListe)
+maListe.append(8)
+maListe.append(5.6)
+print(maListe)
+#maListe.index(8)
+
+print(maListe)
+
+maListe.index(1)
+maListe.sort()
+print(maListe)
+
+texte = texte.replace("a","i")
+print(texte)
+
+for element in texte:
+    texte = texte.replace(element,"*")
+
+print(texte)
+
+print = "une/autre/phrase"
+caractere = "éléphant"
+
+phrase = phrase.replace("/"," ")
+
+for accent in "éàè"
+    caractere = caractere.replace()
+
+print(phrase)
+print(caractere)
